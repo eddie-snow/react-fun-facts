103186cb9d7871a823ea5bc14dee06a81be28783
eddie <107451363+eddie-snow@users.noreply.github.com>
2024-01-26 13:18:35 -0100

    added files
    react course basics, simple project

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+
+import React from "react"
+
+export default function Main () {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>was first released in 1984</li>
+                <li>was originally created by SpongeBob</li>
+                <li>Has over -10k starts on GitHub</li>
+                <li>is maintained by EvilCorp</li>
+                <li>Powers thousands of criminal gangs, including serial killers</li>
+            </ul>
+            
+        </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+
+export default function Navbar () {
+    return (
+        <nav>
+            <img src="../images/react-logo-small.png" alt="LOGO" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts </h3>
+            <h4 className="nav--title">React Course - project 1</h4>
+        </nav>
+    )
+}
+
